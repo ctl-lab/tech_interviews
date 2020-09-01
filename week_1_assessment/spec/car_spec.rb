c1a18fd74b63ef7edb5d6208f5cab5d30ea15c47
emiliano-ma <emilianomainero@gmail.com>
2020-09-01 14:34:03 +0200

    changing the name of the instance double

--- week_1_assessment/spec/car_spec.rb
+++ week_1_assessment/spec/car_spec.rb
@@ -4,10 +4,10 @@ describe Car do
   it 'paints a car' do
     expect(subject.paint_car('red')).to eq 'red'
   end
-  let(:driver) {instance_double('driver', name: 'Emiliano') }
+  let(:my_driver_instance_of_Driver) {instance_double('Driver', name: 'Emiliano') }
 
   it 'car has a driver with a name' do
-    expect(subject.sets_car_driver(driver)).to eq 'Emiliano'
+    expect(subject.sets_car_driver(my_driver_instance_of_Driver)).to eq 'Emiliano'
   end
 end
 
